3f6a47a30d4d7b1c681344f9caf08d00761eb615
radicaled <daniel.bianco@outlook.com>
2022-08-29 21:33:43 -0300

    change the mongo uri

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const shortId = require("shortid");
 
 /*Connect to database*/
-mongoose.connect(process.env.URI, {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
